a55731acdffb150a117b6425215bb64fc765a34a
Derek <derek@ttds-MacBook-Air.local>
2018-05-02 14:36:18 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,43 @@
-class Dog
+require 'pry'
+
+class Dog < ActiveRecord::Base
+  # attr_accessor :name, :text
+
+  def initialize(attributes)
+    super(attributes)
+  end
+
+
+  def self.create(row)
+    Dog.new(row)
+  end
+
+  def self.save
+    self.save
+  end
+
+  def self.update(info)
+    self.update(info)
+  end
+
+  def self.find_or_create_by(info)
+
+    result =  Dog.find_by_name(info[:name])
+    if result == nil
+      result = Dog.new(info)
+    end
+    # binding.pry
+
+  end
+
+  def self.find_by_name(name)
+    Dog.find_by(name:name)
+
+  end
+
+  def self.find_by_id(id)
+    Dog.find(id)
+
+  end
+
 end
